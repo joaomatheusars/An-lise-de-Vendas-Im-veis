239a16721c57f6b19704410fa8ca95ccccfb5811
Joao Matheus <joaomatheusars@gmail.com>
2024-02-19 14:41:30 -0300

    feat: create callbacks

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
+import plotly.express as px
 import pandas as pd
 from app import app
 from _map import *
@@ -35,6 +36,37 @@ app.layout = dbc.Container(
     ],
     fluid=True
 )
+
+# -------------------
+# Callbacks
+# -------------------
+
+
+@app.callback([Output('hist-graph', 'figure')],
+              [Input('location-dropdown', 'value'),
+               Input('slider-square-size', 'value'),
+               Input('dropdown-color', 'value')])
+def update_hist(location, square_size, color_map):
+    if location is None:
+        df_intermediate = df_data.copy()
+    else:
+        df_intermediate = df_data[df_data["BOROUGH"] ==
+                                  location] if location != 0 else df_data.copy()
+        size_limit = slider_size[square_size] if square_size is not None else df_data["GROSS SQUARE FEET"].max(
+        )
+        df_intermediate = df_intermediate[df_intermediate["GROSS SQUARE FEET"] <= size_limit]
+
+    hist_fig = px.histogram(df_intermediate, x=color_map, opacity=0.75)
+    hist_layout = go.Layout(
+        margin=go.layout.Margin(l=10, r=0, t=0, b=5),
+        showlegend=False,
+        template="plotly_dark",
+        paper_bgcolor="rgba(0, 0, 0, 0)"
+    )
+
+    hist_fig.layout = hist_layout
+    return hist_fig
+
 
 if __name__ == "__main__":
     app.run(debug=True)
