be5e7e88606145f90945e458bc24b8527ef97ebe
Joao Matheus <joaomatheusars@gmail.com>
2024-02-19 16:30:40 -0300

    fix: improve color distribution

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from dash import html, dcc
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import numpy as np
 import pandas as pd
 from app import app
 from _map import *
@@ -67,9 +68,19 @@ def update_hist(location, square_size, color_map):
 
     hist_fig.layout = hist_layout
 
+    colors_rgb = px.colors.sequential.GnBu
+    df_quantiles = df_data[color_map].quantile(
+        np.linspace(0, 1, len(colors_rgb))).to_frame()
+    df_quantiles = (df_quantiles - df_quantiles.min()) / \
+        (df_quantiles.max() - df_quantiles.min())
+    df_quantiles["colors"] = colors_rgb
+    df_quantiles.set_index(color_map, inplace=True)
+    color_scale = [[i, j] for i, j in df_quantiles["colors"].iteritems()]
+    
     px.set_mapbox_access_token(environ.get("TOKEN"))
     map_fig = px.scatter_mapbox(df_intermediate, lat="LATITUDE", lon="LONGITUDE",
                                 color=color_map.upper(), size="size_m2", size_max=20, zoom=10, opacity=0.4)
+    map_fig.update_coloraxes(colorscale=color_scale)
     map_fig.update_layout(mapbox=dict(center=go.layout.mapbox.Center(lat=mean_lat, lon=mean_long)),
                           template="plotly_dark", paper_bgcolor="rgba(0, 0, 0, 0)",
                           margin=go.layout.Margin(l=10, r=10, t=10, b=10),)
