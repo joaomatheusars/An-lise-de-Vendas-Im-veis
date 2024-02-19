669e480134029614ef736b59135db4d10a950fa7
Joao Matheus <joaomatheusars@gmail.com>
2024-02-19 14:07:19 -0300

    feat: create value filter

--- _controllers.py
+++ _controllers.py
@@ -1,1 +1,19 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
+from app import app
+
+list_of_locations = {
+    "All": 0,
+    "Manhattan": 1,
+    "Bronx": 2,
+    "Brooklyn": 3,
+    "Queens": 4,
+    "Staten Islands": 5,
+}
+
+slider_list = [100, 500, 1000, 10000, 1000000]
+
+controlers = dbc.Row([
+    html.Img(id="logo", src=app.get_asset_url(
+        "logo_dark.png"), style={"widht": "50%"})
+])
